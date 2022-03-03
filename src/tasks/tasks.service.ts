9676fcd4014894318fb3742feeb6b7a78fb78d41
MauricioR998 <mauricio.rivas98@gmail.com>
2022-03-02 23:13:02 -0500

    Task dto added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 import { Task, TaskStatus } from './task.model';
 
@@ -12,7 +13,10 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    console.log(createTaskDto);
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
