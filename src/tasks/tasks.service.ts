8e9080c0d4a4db0367a248fee111094b68ac71ff
MauricioR998 <mauricio.rivas98@gmail.com>
2022-03-02 23:22:46 -0500

    Get a task by ID and delete Task handlers created

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,6 +13,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     console.log(createTaskDto);
     const { title, description } = createTaskDto;
@@ -27,5 +31,9 @@ export class TasksService {
     this.tasks.push(task);
 
     return task;
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
